778ec4dfbf94d6c644eace4893a89487fe4ed8cd
Andrés Dalla Rizza <[andresdallarizza1081@gmail.com]>
2024-04-03 10:42:05 -0300

    heap

--- Hashmap/HashFunc/hashFunc.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-template <class T>
-class hashFunc
-{
-public:
-  virtual int hash(T data) = 0;
-};
--- Hashmap/HashFunc/intHashFunc.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "hashFunc.cpp"
-
-class intHashFunc : public hashFunc<int>
-{
-public:
-  int hash(int data) override { return data; }
-};
